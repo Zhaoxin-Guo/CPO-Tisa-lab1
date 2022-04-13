7d6cfb39f737908188e0a0801341ab2ed3d64ace
Zhaoxin <77062275+Zhaoxin-Guo@users.noreply.github.com>
2022-04-13 22:02:44 +0800

    Update MutableTest.py

--- MutableTest.py
+++ MutableTest.py
@@ -2,6 +2,8 @@ import unittest
 from hypothesis import given
 from Mutable import *
 import hypothesis.strategies as st
+
+
 class TestMutableUnrolled_linked_list(unittest.TestCase):
 
     def test_size(self):
@@ -58,13 +60,78 @@ class TestMutableUnrolled_linked_list(unittest.TestCase):
         lst.remove(0)
         self.assertEqual(lst.to_list(), [])
 
+    def test_remove(self):
+        lst = UnrolledLinkedList()
+        lst.set(0, 'a')
+        lst.set(1, 'b')
+        lst.set(2, 'c')
+        lst.remove(2)
+        self.assertEqual(lst.to_list(), ['a', 'b'])
+        lst.remove(1)
+        self.assertEqual(lst.to_list(), ['a'])
+        lst.remove(0)
+        self.assertEqual(lst.to_list(), [])
+
+    def test_is_member(self):
+        x = ['a', 'b', 'c']
+        lst = UnrolledLinkedList()
+        lst.from_list(x)
+        index = lst.is_member('b')
+        self.assertEqual(index, 1)
+
+    def test_filter(self):
+        def f(x):
+            res = x + 2
+            return res
+
+        x = [3, 4, 5]
+        lst = UnrolledLinkedList()
+        lst.from_list(x)
+        lst.filter(f)
+        self.assertEqual([5, 6, 7], lst.to_list())
+
+    def test_map(self):
+        lst = UnrolledLinkedList()
+        lst.map(str)
+        self.assertEqual(lst.to_list(), [])
+
+        lst = UnrolledLinkedList()
+        lst.from_list([1, 2, 3])
+        lst.map(str)
+        self.assertEqual(lst.to_list(), ["1", "2", "3"])
+
+        lst = UnrolledLinkedList()
+        lst.from_list([1, 2, 3])
+        lst.map(lambda x: x + 1)
+        self.assertEqual(lst.to_list(), [2, 3, 4])
+
+    def test_reduce(self):
+        # sum of empty list
+        lst = UnrolledLinkedList()
+        self.assertEqual(lst.reduce(lambda st, e: st + e, 0), 0)
+
+        # sum of list
+        lst = UnrolledLinkedList()
+        lst.from_list([1, 2, 3])
+        self.assertEqual(lst.reduce(lambda st, e: st + e, 0), 6)
+        # size
+        test_data = [
+            [],
+            ['a'],
+            ['a', 'b']
+        ]
+        for e in test_data:
+            lst = UnrolledLinkedList()
+            lst.from_list(e)
+            self.assertEqual(lst.reduce(lambda st, _: st + 1, 0), lst.size())
+
+
     @given(st.lists(st.integers()))
     def test_from_list_to_list_equality(self, a):
         lst = UnrolledLinkedList()
         lst.from_list(a)
         b = lst.to_list()
         self.assertEqual(a, b)
-
 
     @given(st.lists(st.integers()))
     def test_python_len_and_list_size_equality(self, a):
