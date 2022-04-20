262ac2c20e9a8a4951835e4c42efcf35a9204277
Zhaoxin <77062275+Zhaoxin-Guo@users.noreply.github.com>
2022-04-20 22:41:25 +0800

    Create Mutable.py

--- Mutable.py
+++ Mutable.py
@@ -1,9 +1,14 @@
 class Node:
-    def __init__(self, capacity=4):
+    def __init__(self, capacity=5):
         self.next = None
         self.numElements = 0  # The number of elements in the node
         self.elements = [None] * capacity
         self.cap = capacity  # Node capacity
+    #
+    # def __new__(cls, *args, **kwargs):
+    #     if not hasattr(cls, 'instance'):
+    #         cls.instance = super(Node, cls).__new__(cls)
+    #     return cls.instance
 
 
 class UnrolledLinkedList:
@@ -13,6 +18,7 @@ class UnrolledLinkedList:
         node = Node()
         self.head.next = node
         node.next = self.tail
+        self.iter_num = 0
 
     def __str__(self):
         """for str() implementation for printing"""
@@ -21,17 +27,54 @@ class UnrolledLinkedList:
     def __iter__(self):
         return self
 
+    # def __next__(self):
+    #     if self.head.next.numElements == 0:
+    #         raise StopIteration
+    #     else:
+    #         self.iter_num = self.iter_num + 1
+    #         return self.iter_num
+
+    # def __next__(self):
+    #     if self.head.next.numElements == 0:
+    #         raise StopIteration
+    #     else:
+    #         count = self.head.next.numElements
+    #         while count is not 0:
+    #             for i in range(0, self.head.next.numElements):
+    #                 count = count - 1
+    #                 tmp = self.head.next.elements[i]
+    #                 self.iter_list.append(tmp)
+    #                 print( self.iter_list)
+    #                 # print(tmp)
+    #                 return tmp
+    #         self.head = self.head.next
+
     def __next__(self):
-        if self.head.numElements == 0:
+        if self.head.next.numElements == 0:
             raise StopIteration
         else:
-            count = self.head.next.numElements
-            while count is not 0:
-                for i in range(0, self.head.next.numElements):
-                    count = count - 1
-                    tmp = self.head.next.elements[i]
-                    return tmp
-            self.head = self.head.next
+            tmp = self.head.next.elements[self.iter_num]
+            self.iter_num = self.iter_num + 1
+            # print(self.iter_num)
+            # print(self.iter_list)
+            return tmp
+        self.head = self.head.next
+
+    # def __next__(self):
+    #     if self.head.next.numElements == 0:
+    #         raise StopIteration
+    #     else:
+    #         count = self.head.next.numElements
+    #         tmp = []
+    #         while count is not 0:
+    #             for i in range(0, self.head.next.numElements):
+    #                 count = count - 1
+    #                 tmp.append(self.head.next.elements[i])
+    #                 # print(count)
+    #                 print(tmp)
+    #             self.iter_num = self.iter_num + 1
+    #         self.head = self.head.next
+    #         return tmp[self.iter_num]
 
     def size(self):
         return self.total_size
@@ -40,7 +83,6 @@ class UnrolledLinkedList:
         if len(lst) == 0:
             self.head.next = None
             return
-        cur = self.head.next
         for e in reversed(lst):
             self.set(0, e)
 
@@ -53,10 +95,8 @@ class UnrolledLinkedList:
             cur = cur.next
         return res
 
-    def set(self, idx, obj):
-        if idx < 0 or idx > self.total_size:
-            return
-
+    def add(self, obj):
+        idx = self.total_size
         # Find the insertion node and position
         cur = self.head.next
         while idx >= cur.numElements:
@@ -73,6 +113,46 @@ class UnrolledLinkedList:
             node.next = next
 
             # Move the element of the inserted node to the new node
+            move_idx = cur.numElements // 2
+            for i in range(move_idx, cur.numElements):
+                node.elements[i - move_idx] = cur.elements[i]
+                cur.elements[i] = None
+                cur.numElements -= 1
+                node.numElements += 1
+
+            # Update the insert position
+            if idx >= move_idx:
+                idx -= move_idx
+                cur = node
+
+        # Insert element
+        for i in range(cur.numElements - 1, idx - 1, -1):
+            cur.elements[i + 1] = cur.elements[i]
+        cur.elements[idx] = obj
+
+        cur.numElements += 1
+        self.total_size += 1
+
+    def set(self, idx, obj):
+        if idx < 0 or idx > self.total_size:
+            return
+
+        # Find the node and position of the removed element
+        cur = self.head.next
+        while idx >= cur.numElements:
+            if idx == cur.numElements:
+                break
+            idx -= cur.numElements
+            cur = cur.next
+
+        if cur.numElements == cur.cap:
+            # The insert node is full, create new node
+            node = Node()
+            next = cur.next
+            cur.next = node
+            node.next = next
+
+            # Move the inserted node general element to the new node
             move_idx = cur.numElements // 2
             for i in range(move_idx, cur.numElements):
                 node.elements[i - move_idx] = cur.elements[i]
@@ -143,10 +223,26 @@ class UnrolledLinkedList:
                     return index
             return -1
 
+    def reverse(self):
+        lst = self.to_list()
+        lst.reverse()
+        lst_new = UnrolledLinkedList()
+        lst_new.from_list(lst)
+        return lst_new
+
+    # def filter(self, f):
+    #     cur = self.head.next
+    #     for i in range(0, cur.numElements):
+    #         cur.elements[i] = f(cur.elements[i])
+    #     return self.to_list()
+
     def filter(self, f):
         cur = self.head.next
-        for i in range(0, cur.numElements):
-            cur.elements[i] = f(cur.elements[i])
+        for i in range(0, cur.numElements // 2 + 1):
+            if not f(cur.elements[i]):
+                del cur.elements[i]
+                cur.numElements -= 1
+
         return self.to_list()
 
     def map(self, f):
@@ -164,3 +260,54 @@ class UnrolledLinkedList:
                 state = f(state, cur.elements[i])
             cur = cur.next
         return state
+
+    def empty(self):
+        return []
+
+    def concat(self, lst1, lst2):
+        lst1.extend(lst2)
+        ans = 0
+        i = 0
+        while i < len(lst1):
+            ans *= lst1[i]
+            i = i + 1
+        lst = UnrolledLinkedList()
+        lst.from_list(lst1)
+        return lst, ans
+
+    # def concat(self, lst1, lst2):
+    #     # if not lst1:
+    #     #     return lst2
+    #     # if not lst2:
+    #     #     return lst1
+    #     i = 0
+    #     sum1= 0
+    #     sum2 = 0
+    #     res = []
+    #     lst1.reduce(lambda st, e: st + e, 0)
+    #     while (i < len(lst2)):
+    #         sum2 = sum2 + lst2[i]
+    #         i = i + 1
+    #     res = sum1 * sum2
+    #     return res
+
+    # def concat(self, lst1, lst2):
+    #     if not lst1:
+    #         return lst2
+    #     if not lst2:
+    #         return lst1
+    #     cur1 = lst1.head.next
+    #     cur2 = lst2.head.next
+    #     lst = UnrolledLinkedList()
+    #     while cur1 is not None and cur2 is not None:
+    #         if cur1.numElements >= cur2.numElements:
+    #             for i in range(0, cur2.numElements):
+    #                 lst.add(cur1.elements[i] * cur2.elements[i])
+    #             cur1 = cur1.next
+    #             cur2 = cur2.next
+    #         else:
+    #             for i in range(0, cur1.numElements):
+    #                 lst.add(cur1.elements[i] * cur2.elements[i])
+    #             cur1 = cur1.next
+    #             cur2 = cur2.next
+    #     return lst
